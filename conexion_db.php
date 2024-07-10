4ff4b9d2f992654549d8314114ded924b522d681
Pau-537 <131034457+Pau-537@users.noreply.github.com>
2024-07-10 20:31:27 -0300

    Add files via upload
    Implementación de conexión con base de datos

---
+++ conexion_db.php
@@ -0,0 +1,18 @@
+<?php
+$server = "localhost";
+$usuario = "admin";
+$contrasena = "admin";
+$nombrebd = "heracles_bd";
+$conexion = new mysqli($server, $usuario, $contrasena, $nombrebd);
+if ($conexion->connect_error) {
+    die("Falló la conexión " . $conexion->connect_error);
+} else {
+    if ($_GET["opcion"] == "login") {
+        header("location:login.html");
+    } else {
+        header("location:registro.html");
+    }
+    exit();
+}
+
+?>
