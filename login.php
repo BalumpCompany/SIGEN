67cd6560f7eac069e3b9513a92221f00e4ed80bc
Pau-537 <131034457+Pau-537@users.noreply.github.com>
2024-08-11 19:15:06 -0300

    Inclusion de login y registro en BD

--- login.php
+++ login.php
@@ -1,11 +1,11 @@
 <?php
 include("conexion_db.php");
-$queryLogin="SELECT Contrasena FROM Usuario WHERE Username='".$_GET["user"]."';";
-$contra=$conexionBDUsuario->query($queryLogin)->fetch_assoc();
-var_dump($contra["Contrasena"]) ;
+$queryLogin="SELECT Contrasena,Rol FROM Usuario WHERE Username='".$_GET["user"]."';";
+$fetch=$conexionBDUsuario->query($queryLogin)->fetch_assoc();
+var_dump($fetch["Contrasena"]) ;
 var_dump($_GET["pass"]);
-if ($_GET["pass"]===$contra["Contrasena"]) {
-    header("Location:ventanaCliente.php?user=paulo");
+if ($_GET["pass"]===$fetch["Contrasena"]) {
+    header("Location:ventana".$fetch["Rol"].".php?user=".$_GET["user"]);
 }else{
     header("Location:login.html");
 }
