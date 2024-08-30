3922c8d820131987bd0de66643534fc0c53b2f5b
Pau-537 <131034457+Pau-537@users.noreply.github.com>
2024-08-29 21:14:20 -0300

    Implementación de base de datos unificada.

--- Negocio/conexion_db.php
+++ Negocio/conexion_db.php
@@ -2,12 +2,8 @@
 $server = "localhost";
 $usuario = "root";
 $contrasena = "";
-$nombreBDUsuario = "heracles_usuario";
-//$_SESSION["conexionBD"] = new mysqli($server, $usuario, $contrasena, $nombreBD);
+$nombreBDUsuario = "heracles_bd";
 $conexionBDUsuario = new mysqli($server, $usuario, $contrasena, $nombreBDUsuario);
-/*if ($_SESSION["conexionBD"]->connect_error) {
-    die("Falló la conexión " . $_SESSION["conexionBD"]->connect_error);
-}*/
 if ($conexionBDUsuario->connect_error) {
     die("Falló la conexión " . $conexionBDUsuario->connect_error);
 }
