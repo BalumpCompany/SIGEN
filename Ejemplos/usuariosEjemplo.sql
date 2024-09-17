0cc803c8cb54bf34c577d523827a7a6220e6523a
Pau-537 <131034457+Pau-537@users.noreply.github.com>
2024-09-17 11:32:25 -0300

    Update usuariosEjemplo.sql

--- Ejemplos/usuariosEjemplo.sql
+++ Ejemplos/usuariosEjemplo.sql
@@ -3,5 +3,5 @@ INSERT INTO usuario VALUES ('admin', 'Usuario2', 'Admin', 'Admin@gmail.com', 'Co
 INSERT INTO usuario VALUES ('coach', 'Usuario3', 'Coach', 'coach@gmail.com', 'Contrasena', 'Coach');
 INSERT INTO usuario VALUES ('avanzado', 'Usuario4', 'Avanzado', 'avanzado@gmail.com', 'Contrasena', 'Avanzado');
 INSERT INTO usuario VALUES ('seleccionador', 'Usuario5', 'Seleccionador', 'seleccionador@gmail.com', 'Contrasena', 'Seleccionador');
-INSERT INTO usuario VALUES ('admin it', 'Usuario6', 'AdminIT', 'adminIT@gmail.com', 'Contrasena', 'AdminTI');
+INSERT INTO usuario VALUES ('adminit', 'Usuario6', 'AdminIT', 'adminIT@gmail.com', 'Contrasena', 'AdminTI');
 
