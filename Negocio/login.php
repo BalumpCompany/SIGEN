79c6abae0ad286210c94b632472a1c76afe0b6c3
lome301 <maxiust@hotmail.com>
2024-09-17 22:19:54 -0300

    Encriptado de contraseñas

--- Negocio/login.php
+++ Negocio/login.php
@@ -3,14 +3,16 @@ require '../Datos/Usuario.php';
 require '../Datos/UsuarioRepo.php';
 session_start();
 $repo = new UsuarioRepo();
-$usuario = $repo->obtener($_POST["user"])[0];
-if ($_POST["pass"]===$usuario->Contrasena) {
+$usuario = $repo->obtener($_POST["user"], $_POST["pass"])[0];
+
+if (isset($usuario->Contrasena)) {
     header("Location: ../Presentacion/ventana".$usuario->Rol.".php?user=".$_POST["user"]);
     $_SESSION["logueado"]=true;
     $_SESSION["user"]=$_POST["user"];
     $_SESSION["rol"]=$usuario->Rol;
 }else{
-    header("Location: ../Presentacion/login.html");
+    //header("Location: ../Presentacion/login.html");
+    echo "<script>alert($usuario)</script>";
 }
 exit();
 ?>
