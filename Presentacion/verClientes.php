c627c63da6bc90955f12bb6bccd3d66fc8346443
Max ust <vaquinomaximiliano89@gmail.com>
2024-10-19 23:02:58 -0300

    Work in progress - Agregada la ventana admin y sus funcionalidades

--- Presentacion/verClientes.php
+++ Presentacion/verClientes.php
@@ -58,6 +58,58 @@ if($_SESSION["logueado"]==true && $_SESSION["rol"]=="Coach"){
 </body>
 </html>
 <?php
+}elseif($_SESSION["logueado"]==true && $_SESSION["rol"]=="Admin"){
+
+    require '../Datos/ClienteRepo.php';
+    $repo = new ClienteRepo();
+    $clientes= $repo->obtenerTodos();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pantalla principal - <?php echo $_GET["user"]; ?></title>
+    <link rel="stylesheet" href="styleVerEjercicios.css">
+    <link rel="icon" href="recursos/icono.png">
+</head>
+<body>
+    <nav>
+        <a href="index.php"><img src="recursos/header_beige.jpg" alt="Heracles" id="imgNav"></a>
+        <form action="../Negocio/cerrarSesion.php" method="post" onsubmit="return confirmacion()"><button id="cerrarSesion">Cerrar sesión</button></form>
+    </nav>
+    <div id="barraLateral">
+        <a href="verClientes.php?user=<?php echo $_GET["user"]; ?>"><p>Ver clientes</p></a>
+        <a href="verSucursales.php?user=<?php echo $_GET["user"]; ?>"><p>Ver sucursales</p></a>
+        <a href="crearSucursal.php?user=<?php echo $_GET["user"]; ?>"><p>Crear sucursales</p></a>
+        <a href="verPagos.php?user=<?php echo $_GET["user"]; ?>"><p>Ver pagos</p></a>
+    </div>
+    <div id="contenidoPrincipal">
+    <h1>Listado de clientes</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>N° Socio</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($clientes as $cliente):?>
+                <tr>
+                    <td><strong><?php echo htmlspecialchars($cliente->Numero_Socio); ?></strong></td>
+                    <td><?php echo htmlspecialchars($cliente->Nombre); ?></td>
+                    <td><?php echo $cliente->Apellido; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+    </div>
+    <script src="jquery-3.7.1.min.js"></script>
+    <script src="confirmacion.js"></script>
+
+<?php
 }else{
     header("Location:index.php");
     exit();
