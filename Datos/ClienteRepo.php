7cd54649e85ae7440482bba45fc9ddb93f3191a1
Bauticool1 <117576093+Bauticool1@users.noreply.github.com>
2024-11-02 18:09:02 -0300

    Cree la ventana verCalificaciones

--- Datos/ClienteRepo.php
+++ Datos/ClienteRepo.php
@@ -80,5 +80,17 @@ class ClienteRepo
         return $result;
     }
 
+    public function obtenerPuntuacion($user)
+    {
+        $numero = $this->conexion->query("SELECT Numero_Socio FROM escliente WHERE Username='$user'")->fetch_array();
+        $numeroSocio = $numero['Numero_Socio']; 
+        $resultado = $this->conexion->query("SELECT * FROM califica WHERE Numero_Socio=$numeroSocio"); // Traigo todas las Puntuaciones de la base de datos
+        $retorno = []; //Arreglo auxiliar
+        while ($Cliente = $resultado->fetch_object()) { //Voy convirtiendo, uno por uno, los resultados en objetos de la clase stdClass
+            $retorno[] = $Cliente; //Agrego los objetos al arreglo auxiliar
+        }
+        return $retorno; //Devuelvo el arreglo
+    }
+
 }
 ?>
